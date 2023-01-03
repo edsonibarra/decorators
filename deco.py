b5bb1402a4c8c0aa800240e96bae01b961ee9c24
edson ibarra <edsonibarra97@gmail.com>
2023-01-02 21:53:56 -0600

    Example Working

--- deco.py
+++ deco.py
@@ -1,15 +1,26 @@
+def int_validation(func):
+    def inner(*args):
+        x: int = args[0]
+        y: int = args[1]
+        if isinstance(x, int) and isinstance(y, int):
+            result = func(x, y)
+        else:
+            raise ValueError(f'Only int values are accepted. Values provided {x}, {y} are invalid')
+        return result
+    return inner
+
+
+@int_validation
 def sum_two_integers(x: int, y: int) -> int:
-    if isinstance(x, int) and isinstance(y, int):
         return x + y
-    else:
-        raise ValueError('Only int values are accepted')
 
 
 def main():
     x: int = 100
     y: int = 300
 
-    result: int = sum_two_integers(x, y)
+    # result: int = sum_two_integers('hello ', 'world')
+    result: int = sum_two_integers(100, 500)
 
     print(result)
 
